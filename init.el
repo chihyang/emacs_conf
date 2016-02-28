e3ad46a865819ae28fd461c171ff1fa6ea8e312f
chihyang <chihyanghsin@gmail.com>
2016-02-28 21:05:14 +0800

    Add utf-8 preference.
    There is some problem with magit when every buffer uses utf-8 as default. So only paste preference is set to utf-8 for now.

--- init.el
+++ init.el
@@ -19,6 +19,18 @@
 (desktop-save-mode 1)     ; save sessions
 (setenv "GIT_ASKPASS" "git-gui--askpass") ; set git for pushing to github by https
 (delete-selection-mode 1)                 ; delete selection mode
+(setq column-number-mode t)               ; enable column-number-mode
+;; (prefer-coding-system 'utf-8)             ; set default encoding as utf-8
+;; (set-default-coding-systems 'utf-8)
+;; (set-terminal-coding-system 'utf-8)
+;; (set-keyboard-coding-system 'utf-8)
+;; backwards compatibility as default-buffer-file-coding-system
+;; is deprecated in 23.2.
+;; (if (boundp 'buffer-file-coding-system)
+;;     (setq-default buffer-file-coding-system 'utf-8)
+;;   (setq default-buffer-file-coding-system 'utf-8))
+;; Treat clipboard input as UTF-8 string first; compound text next, etc.
+(setq x-select-request-type '(UTF8_STRING COMPOUND_TEXT TEXT STRING))
 ;; change hot-key
 (global-set-key (kbd "M-9") 'kill-whole-line) ; delete a whole line with M-9
 
@@ -30,7 +42,8 @@
  ;; If there is more than one, they won't work right.
  '(ecb-options-version "2.40")
  '(markdown-command "pandoc -f markdown_github")
- '(session-use-package t nil (session)))
+ '(session-use-package t nil (session))
+ '(speedbar-show-unknown-files t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
