d4cb92aca13518cc215a88b999f98f771062fc45
chihyang <chihyanghsin@gmail.com>
2016-05-03 19:02:29 +0800

    Install irony-server for C family.
    To install irony-server, use the command:
    "cmake" -G "Visual Studio 12 2013 Win64"
            "-DCMAKE_PREFIX_PATH=D:/ProgramDevelop/LLVM"
            "-DCMAKE_INSTALL_PREFIX=d:/ProgramDevelop/EMACS/etc/.emacs.d/irony/"
            "d:/ProgramDevelop/EMACS/etc/.emacs.d/elpa/irony-20160317.1527/server"
            && "cmake" --build . --use-stderr --config Release --target install

--- init-package-elpa.el
+++ init-package-elpa.el
@@ -26,6 +26,7 @@
     cursor-chg
     ecb
     haskell-mode
+    irony
     magit
     markdown-mode
     markdown-preview-mode
@@ -77,6 +78,21 @@
  '(sr-speedbar-default-width 100)
  '(sr-speedbar-max-width 100))
 (tabbar-mode)                                 ; tab-bar
+
+(add-hook 'c++-mode-hook 'irony-mode)
+(add-hook 'c-mode-hook 'irony-mode)
+(add-hook 'objc-mode-hook 'irony-mode)
+(add-to-list 'exec-path "D:/ProgramDevelop/LLVM/bin")
+;; replace the `completion-at-point' and `complete-symbol' bindings in
+;; irony-mode's buffers by irony-mode's function
+(defun my-irony-mode-hook ()
+  (define-key irony-mode-map [remap completion-at-point]
+    'irony-completion-at-point-async)
+  (define-key irony-mode-map [remap complete-symbol]
+    'irony-completion-at-point-async))
+(add-hook 'irony-mode-hook 'my-irony-mode-hook)
+(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+
 ;; enable some minor modes globally
 (define-globalized-minor-mode my-global-minor-mode auto-highlight-symbol-mode
   (lambda () (auto-highlight-symbol-mode 1)))
