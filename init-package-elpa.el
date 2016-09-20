f922a40a563f6e95c5fe26a2cea58053eddd892b
chihyang <chihyanghsin@gmail.com>
2016-09-20 17:23:25 +0800

    Rebind yasnippet trigger key.

--- init-package-elpa.el
+++ init-package-elpa.el
@@ -265,6 +265,13 @@
 ;; yasnippet
 (yas-global-mode 1)
 (add-hook 'prog-mode-hook #'yas-minor-mode)
+;; Remove Yasnippet's default tab key binding
+(define-key yas-minor-mode-map (kbd "<tab>") nil)
+(define-key yas-minor-mode-map (kbd "TAB") nil)
+;; Set Yasnippet's key binding to shift+tab
+(define-key yas-minor-mode-map (kbd "<backtab>") 'yas-expand)
+;; Alternatively use Control-c + tab
+(define-key yas-minor-mode-map (kbd "\C-c TAB") 'yas-expand)
 
 (provide 'init-package-elpa)
 ;;; init-package-elpa.el ends here
