1c4604abdbe0219ad07c4bd7b7691a6cc6a7e7cd
chihyang <chihyang@gmail.com>
2017-07-20 15:00:55 +0800

    Change key binding for avy-goto-char.

--- init-package-elpa.el
+++ init-package-elpa.el
@@ -180,8 +180,8 @@
     (autopair-global-mode 1))
 
 ;; avy
-(global-set-key (kbd "C-:") 'avy-goto-char)
-(global-set-key (kbd "C-'") 'avy-goto-char-2)
+(global-set-key (kbd "M-g :") 'avy-goto-char)
+(global-set-key (kbd "M-g '") 'avy-goto-char-2)
 (global-set-key (kbd "M-g f") 'avy-goto-line)
 (global-set-key (kbd "M-g w") 'avy-goto-word-1)
 (global-set-key (kbd "M-g e") 'avy-goto-word-0)
