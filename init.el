fc125dabf454dd31ea556c3ec328ed01aca52dd5
chihyang <chihyanghsin@gmail.com>
2018-01-10 12:14:57 +0800

    Put customization items into a separate file.

--- init.el
+++ init.el
@@ -194,16 +194,6 @@
   (setq load-path
         (update-path-list-iter load-path '())))
 
-;; custom parameter
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(setq custom-file "~/.emacs.d/.emacs-custom.el")
+(when (file-exists-p custom-file)
+  (load custom-file))
