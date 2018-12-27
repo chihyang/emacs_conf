4dd46e93367cccf47730969d11cbf2e087034515
chihyang <chihyanghsin@gmail.com>
2018-12-27 12:07:06 +0800

    Modify dired time format.

--- init-package-builtin.el
+++ init-package-builtin.el
@@ -110,7 +110,7 @@
 ;; dried omit mode
 (require 'dired-x)
 (setq dired-omit-files (concat dired-omit-files "\\|^\\..+$"))
-(setq dired-listing-switches "-alh")
+(setq dired-listing-switches "-alh --time-style \"+%Y-%m-%d %H:%M:%S\"")
 
 ;; ede
 (global-ede-mode 1)                      ; Enable the Project management system
